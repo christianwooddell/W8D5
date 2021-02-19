08e817c257561fe0fafe1d845047bfd70ef88188
Christian <cwooddel@u.rochester.edu>
2021-02-19 16:04:03 -0500

    Finish clock kind of

--- introtocallbacks.js
+++ introtocallbacks.js
@@ -1,19 +1,22 @@
 class Clock {
     constructor() {
-        // 1. Create a Date object.
-        // 2. Store the hours, minutes, and seconds.
-        // 3. Call printTime.
-        // 4. Schedule the tick at 1 second intervals.
+        let date = new Date() 
+        this.hours = date.getHours();
+        this.minutes = date.getMinutes();
+        this.seconds = date.getSeconds();
+        
+        this.printTime()
+        setInterval (this._tick.bind(this), 1000);
     }
 
     printTime() {
-        // Format the time in HH:MM:SS
-        // Use console.log to print it.
+        console.log(`the time is: ${this.hours}:${this.minutes}:${this.seconds}!`)
     }
 
     _tick() {
-        // 1. Increment the time by one second.
-        // 2. Call printTime.
+        this.seconds++ 
+        
+        this.printTime();
     }
 }
 
